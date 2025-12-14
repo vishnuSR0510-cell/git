2a918d7e1a1fe43489e76a7e23373e0c88daff0b
project <vishnuchandrasekar6313@gmail.com>
2025-12-14 17:09:23 +0530

    commit change-1

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ for(var i=0;i<=5;i++){
     console.log("vishnu web developer")
 }
 
-console.log("iam the rep"){
-    console.log("vishnusr")
-}
+
 console.log("iam the senior of the college okay ")
 console.log("iam the 4 year eee rep ")
